9ce3ac4db144743604cf54db018644399d9122fc
Adareen <38138159+Adareen@users.noreply.github.com>
2025-03-09 19:29:13 +0100

    .

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5490, hash: 'e2e4eba8a87fa8562e1884592499c0ffd0efcca2ea9c7d6331e04eae6a5b67b1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1579, hash: 'bbe2c21f30604bd82e10e4d6cf498d52ace4ef1ce84d386202aaaba88411c0f3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 18496, hash: '5272e08b1315b45b5be41b158ef47f3e8155ba7984c3afb60c0c6be7dcf9f86c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5490, hash: '6a9473453605dfc9fddf956371692f580493b9fbbf06dbb99106a8f2bc338656', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1579, hash: '6800388a5ae0760a866f6e1908197c78e07294642033384bfca6e79345cdb7cd', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 18458, hash: 'cece78b584f92b214f680c5e60bc55097b6d387a2fa353520e9eb7b005370273', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-LAK7BA7D.css': {size: 232012, hash: 'TiCFRPGhqz0', text: () => import('./assets-chunks/styles-LAK7BA7D_css.mjs').then(m => m.default)}
   },
 };
